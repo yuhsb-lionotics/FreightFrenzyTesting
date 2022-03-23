466eec978174212198c3d1e883f002b235991e3d
Lionotics <48888106+YUHSB@users.noreply.github.com>
2022-03-22 20:13:08 -0400

    A start?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Hardware extends LinearOpMode {
+    protected DcMotor frontLeft, frontRight, backLeft, backRight;
+    @Override
+    public void runOpMode() throws InterruptedException {
+    }
+}
