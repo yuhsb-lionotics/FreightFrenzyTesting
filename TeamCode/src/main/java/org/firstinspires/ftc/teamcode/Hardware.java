a22199b7514e559ea644794702a132e672c2e4dc
daniel-farkas-dev <54506760+daniel-farkas-dev@users.noreply.github.com>
2022-03-24 15:32:18 -0400

    Defined wheel objects

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -4,7 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Hardware extends LinearOpMode {
+    //Wheels
     protected DcMotor frontLeft, frontRight, backLeft, backRight;
+    frontLeft = hardwareMap.dcMotor.get("frontLeft");
+    frontRight = hardwareMap.dcMotor.get("frontRight");
+    backRight = hardwareMap.dcMotor.get("backRight");
+    backLeft = hardwareMap.dcMotor.get("backLeft");
+    
     @Override
     public void runOpMode() throws InterruptedException {
     }
